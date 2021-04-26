68bc13690121122263dc0ba5afb7eab37663f52b
JaidenRuiz <JaidenRuiz16@gmail.com>
2021-04-26 17:08:58 +0000

    add runtime project

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,11 +16,25 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [
-                { "type": "spike", "x": 400, "y": groundY },
-                { "type": "spike", "x": 600, "y": groundY },
-                { "type": "spike", "x": 900, "y": groundY },
+                { "type": "spike", "x": 1300, "y": groundY },
+                { "type": "spike", "x": 2000, "y": groundY },
+                { "type": "spike", "x": 500, "y": groundY },
+                { "type": "enemy", "x": 900, "y": 500 },
+                { "type": "enemy", "x": 700, "y": 500 },
+                { "type": "enemy", "x": 600, "y": 500 },
             ]
         };
+    
+        for (var key = 0; key < levelData.gameItems.length; key++){
+             var gameItemsObject = levelData.gameItems[key];
+             if (gameItemsObject.type === 'spike'){
+                 createSpike(gameItemsObject.x, gameItemsObject.y);
+             } else {
+                 createEnemy(gameItemsObject.x, gameItemsObject.y);
+             }
+        }
+
+
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
         game.setDebugMode(true);
@@ -28,85 +42,48 @@ var level01 = function (window) {
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
         
-        function createSpikes(x, y){
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var spikesHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-        spikesHitZone.x = 400;
-        spikesHitZone.y = 375;
-
-        game.addGameItem(spikesHitZone);
-
-        var obstacleImage = draw.bitmap('img/spike.png');
-        spikesHitZone.addChild(obstacleImage);
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        }
-
-        for (var key = 0; key < levelData.gameItems.length; key++){
-             var gameItemObject = levelData.gameItems[key];
-             if (gameItemObject.type === 'spikes'){
-                 craeteSpikes(gameItemsObject.x, gameItemObject.y);
-             }
-        }
-
-
-
-
-        if (gameItemObject.type === 'spike'){
-            createSpikes(gameItemObject.x, gameItemObject.y);
-        }
-
-
-
-        
-        function createSpikes(x,y) {
+      
+        function createSpike(x,y) {
             var hitZoneSize = 25;
             var damageFromObstacle = 10;
             var spikeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
             spikeHitZone.x = x;
             spikeHitZone.y = y;
 
-            game.addGameItem(spikesHitZone);
+            game.addGameItem(spikeHitZone);
 
             var obstacleImage = draw.bitmap('img/Spike.png');
-            spikesHitZone.addChild(obstacleImage);
+            spikeHitZone.addChild(obstacleImage);
             obstacleImage.x = -25;
             obstacleImage.y = -25;
             obstacleImage.scaleX = .4;
             obstacleImage.scaleY = .4;
         }
         
-       function createEnemy(x, y){
-        var enemy = game.craeteGameItem('enemy', 25);
-        var redSquare = draw.rect(50, 50, 'red');
-        redSquare.x = -25;
-        redSquare.y = -25;
-        enemy.addChild(redsquare);
+        function createEnemy(x, y){
+            var enemy = game.craeteGameItem('enemy', 25);
+            var redSquare = draw.rect(50, 50, 'red');
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redsquare);
 
-        enemy.x = 400;
-        enemy.y = groundY - 50;
-        game.addGameItem(enemy);
+            enemy.x = 400;
+            enemy.y = groundY - 50;
+            game.addGameItem(enemy);
 
-        enemy.velocityX = -1;
+            enemy.velocityX = -1;
 
-        enemy.onPlayerCollision = function (){
-            game.cahngeIntegrity(-30);
-            enemy.fadeOut();
-        };
-        
-        enemy.onProjectileCollision = function (){
-            game.increaseScore(50);
-            enemy.fadeOut();
-        };
-        
-        
-        
-        
-        
-        createSpikes(141, groundY);
+            enemy.onPlayerCollision = function (){
+                game.cahngeIntegrity(-30);
+                enemy.fadeOut();
+            };
+            
+            enemy.onProjectileCollision = function (){
+                game.increaseScore(50);
+                enemy.fadeOut();
+            };
+        }
     }
-
 
 };
         // DO NOT EDIT CODE BELOW HERE
